acb4b1eaeb700e43072be186fda1ab764092b712
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-10-25 17:39:24 +0530

    Making store to controll all type of api

--- Client/src/redux/api/userAPI.ts
+++ Client/src/redux/api/userAPI.ts
@@ -1,3 +1,12 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { server } from "../store";
 
-export const userAPI = createApi()
+export const userAPI = createApi({
+  reducerPath: "userAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/user` }),
+  endpoints: (builder) => ({
+    login: builder.mutation<string, number>({
+      query: (user) => ({ url: "new", method: "POST", body: user }),
+    }),
+  }),
+});
